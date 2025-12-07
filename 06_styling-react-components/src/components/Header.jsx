dea758d017118c72c95fbfa53a8a930ce87d0a2a
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-07 12:43:18 +0530

    Header styling done with tailwind css

--- 06_styling-react-components/src/components/Header.jsx
+++ 06_styling-react-components/src/components/Header.jsx
@@ -1,14 +1,21 @@
 /** @format */
 
 import logo from '../assets/logo.png';
-import './Header.css';
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt='A canvas' />
-      <h1>ReactArt</h1>
-      <p style={{ color: 'red' }}>A community of artists and art-lovers.</p>
+    <header className='flex flex-col items-center justify-center mt-4 mb-4 md:mb-16'>
+      <img
+        src={logo}
+        className='object-contain mb-4 w-44 h-44'
+        alt='A canvas'
+      />
+      <h1 className='text-4xl font-bold text-center uppercase tracking-wider font-pacifico m-0 text-red md:text-4xl'>
+        ReactArt
+      </h1>
+      <p className='text-center m-0 text-grey'>
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
