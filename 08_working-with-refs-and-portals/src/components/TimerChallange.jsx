d4428be94a433f880c442ac1df770330f2a8a85b
Yogesh Dhuri <ysdhuri312@gmail.com>
2025-12-08 12:10:52 +0530

    Using ref for more

--- 08_working-with-refs-and-portals/src/components/TimerChallange.jsx
+++ 08_working-with-refs-and-portals/src/components/TimerChallange.jsx
@@ -1,16 +1,39 @@
 /** @format */
 
+import { useRef, useState } from 'react';
+
 const TimerChallange = ({ title, targetTime }) => {
+  const timer = useRef();
+
+  const [timeStarted, setTimeStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleClick() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimeStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimeStarted(false);
+  }
+
   return (
     <section className='challenge'>
       <h2>{title}</h2>
+      {timerExpired ? <p>You lost!</p> : ''}
       <p className='challenge-time'>
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>Start Chanllenge</button>
+        <button onClick={timeStarted ? handleStop : handleClick}>
+          {timerExpired ? 'Stop' : 'Start'} Challenge
+        </button>
       </p>
-      <p>Timmer running...</p>
+      <p>{timeStarted ? 'Timmer running...' : 'Timer stopped...'}</p>
     </section>
   );
 };
